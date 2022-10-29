82da300dda25d2a9f4ea891a6c10ed16e1fee637
Boxvi <hermosokawai@gmail.com>
2022-10-28 23:59:00 -0500

    first commit

---
+++ src/main/java/vcardboris/VcardborisApplication.java
@@ -0,0 +1,13 @@
+package vcardboris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcardborisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcardborisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vcardboris/VcardborisApplicationTests.java
@@ -0,0 +1,13 @@
+package vcardboris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcardborisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
